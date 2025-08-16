968d3be3433aa0e9538e1deda0fc0d17ebb8a1ba
Vachek-hooves <vachek0602@gmail.com>
2025-08-16 13:41:58 +0300

    fine

---
+++ config/credentials.js
@@ -0,0 +1,28 @@
+// appFlyer initSdk options
+
+export const OPTIONS = {
+  devKey: 'qxWEmiwRWjPyqJEuqcExBY',
+  // appId: '',
+  onInstallConversionDataListener: true,
+  onDeepLinkListener: true,
+  timeToWaitForATTUserAuthorization: 10,
+  manualStart: true,
+};
+
+// secret FB key -b525200b88b1bc6e3b62abb6180c1504
+
+// FB app cred
+export const FB_APP_ID = '1292404435721549';
+export const FB_CLIENT_TOKEN = '904945f36f1375c075978f7c9f5d98b0';
+
+// Compaign
+//https://willow-moss-stream.space/zrIeTXts
+export const INITIAL_URL = `https://willow-moss-stream.space/`;
+export const URL_IDENTIFAIRE = `zrIeTXts`;
+
+export const ONE_SIGNAL_ID = '1308c631-7528-4d0e-b928-dbfc7639a34f';
+
+export const USER_AGENT =
+  '"Mozilla/5.0 (Linux; Android 13; Mobile) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36"';
+
+// s2s token - e3d3d447-eb50-4397-9da6-2c528fb36395
